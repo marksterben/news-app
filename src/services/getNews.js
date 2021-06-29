dc962d2399904388f020009354b288e8f3a5c868
marksterben <68014273+marksterben@users.noreply.github.com>
2021-06-29 15:17:53 +0700

    Update getNews.js

--- src/services/getNews.js
+++ src/services/getNews.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const NEWS_API_BASE_URL = process.env.REACT_APP_NEWS_API_BASE_URL;
 const NEWS_API_KEY = process.env.REACT_APP_API_KEY;
 
-export const getNews = async (searchQuery = 'technology') => {
+export const getNews = async ({ searchQuery }) => {
   try {
     const path = '/everything';
     const query = `?q=${searchQuery}&apiKey=${NEWS_API_KEY}`;
@@ -12,11 +12,7 @@ export const getNews = async (searchQuery = 'technology') => {
     const res = await axios.get(endpointURL);
 
     if (res.status === 200) {
-      const additionalObj = {
-        category: searchQuery
-      };
-
-      const resData = { ...res.data, ...additionalObj };
+      const resData = { ...res.data, category: searchQuery };
 
       return resData;
     }
